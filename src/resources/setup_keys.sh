a07032be01bea45820b5a7199d072022c614de06
adam hindry <hindryadam@myvuw.ac.nz>
2024-09-30 15:33:03 +1300

    setup key creation

---
+++ src/resources/setup_keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Generating keys!"
+
+# Generate the server keys
+cd client
+openssl req -newkey rsa:2048 -nodes -keyout server-key.pem -x509 -days 365 -out server-certificate.pem
+openssl pkcs12 -inkey server-key.pem -in server-certificate.pem -export -out server-certificate.p12
+
+# Generate the client keys
+cd ../server
+openssl req -newkey rsa:2048 -nodes -keyout client-key.pem -x509 -days 365 -out client-certificate.pem
+openssl pkcs12 -inkey client-key.pem -in client-certificate.pem -export -out client-certificate.p12
